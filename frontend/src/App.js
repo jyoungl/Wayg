c150cafde9669ccdb6e4fe56a5036ac62364231c
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-09-07 11:38:14 +0900

    create react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,13 @@ import {
   Route,
 } from "react-router-dom"
 import Main from "./routes/Main"
-import Login from "./routes/Login"
-import Feed from "./routes/Feed"
-import Course from "./routes/Course"
-import Navigation from "./components/Navigation"
-
 
 function App() {
   return (
   <Router>
-    <Navigation/>
+    <Routes/>
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/feed" element={<Feed />} />
-      <Route path="/course" element={<Course />} />
     </Routes>
   </Router>
   );
