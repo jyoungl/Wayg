4bf9f8f158770bcc21352ae7bf5da1eb99eb4a97
heeejoo0518 <heeejoo0518@gmail.com>
2022-09-14 11:06:09 +0900

    #4 feat 정규화
    라이브러리 Okt로 확정

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,37 +1,36 @@
 from flask import Flask, render_template, jsonify, request
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
-from konlpy.tag import Kkma, Okt, Komoran
+from konlpy.tag import Okt
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")  # Flask 객체에 Api 객체 등록
 
 # morph_api = api.namespace('/', description='형태소 분리 API')
 
-kkma = Kkma()
 okt = Okt()
-komoran = Komoran()
 
 
 @api.route("/morph")
 class Morpheme(Resource):
     @api.doc(params={'text': {'description': '형태소 분리할 문장들', 'type': 'String'}})
     def get(self):
-        ''' 문장들을 받아서 형태소로 분리해 반환한다. (중복제외) '''
+        ''' 텍스트 정규화를 거친 후 형태소로 분리해 반환한다. 중복은 제외한다. '''
         text = request.args.get('text')
         # morphs = okt.morphs(text)  # 형태소 추출
-        # kkma.sentences(text)  # 문장 분리
-
+        # okt.sentences(text)  # 문장 분리
+        text = okt.normalize(text)  # 정규화
         pos = okt.pos(text) # 형태소+품사태그 추출
-        # nouns = kkma.nouns(text) # 명사만 추출
+        # nouns = okt.nouns(text) # 명사만 추출
         return list(set(pos))
 
     def post(self):
-        '''문장들을 받아서 형태소로 분리해 반환한다. (중복제외) '''
+        '''텍스트 정규화를 거친 후 형태소로 분리해 반환한다. 중복은 제외한다.  '''
         # text = request.get_json()['text'] # json으로 받을때
         text = request.form.get('text') # form으로 받을 때
         # text = text.encode("utf-8")
+        text = okt.normalize(text)  # 정규화
         pos = okt.pos(text)  # 형태소+품사태그 추출
-        # nouns = kkma.nouns(text) # 명사만 추출
+        # nouns = okt.nouns(text) # 명사만 추출
         return list(set(pos))
 
 
