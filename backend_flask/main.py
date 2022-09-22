ff0d912448b538dcf58198a3052a641654281c70
heeejoo0518 <heeejoo0518@gmail.com>
2022-09-23 00:12:47 +0900

    #17 feat flask 설정

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -44,5 +44,5 @@ class Morpheme(Resource):
     #     return list(set(pos))
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+#if __name__ == "__main__":
+#    app.run(debug=False, host='0.0.0.0', port=5000)
