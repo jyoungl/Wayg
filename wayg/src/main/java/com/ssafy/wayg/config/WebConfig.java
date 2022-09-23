013546bb07f9458bddbbcb8803ed58b8c24eee1b
허은지 <uiu1442@naver.com>
2022-09-23 13:53:37 +0900

    #17 fix 서버 설정

--- wayg/src/main/java/com/ssafy/wayg/config/WebConfig.java
+++ wayg/src/main/java/com/ssafy/wayg/config/WebConfig.java
@@ -15,8 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 //		Allow all headers.
 //		Set max age to 1800 seconds (30 minutes).
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://j7c202.p.ssafy.io")
+                .allowedOrigins("http://localhost:3000", "https://j7c202.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(6000);
