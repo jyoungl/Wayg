897df414b9684bdbb6af6cd576a1b1aa4fd2a46f
허은지 <uiu1442@naver.com>
2022-09-23 13:45:33 +0900

    #17 feat 서버 설정

--- wayg/src/main/java/com/ssafy/wayg/config/WebConfig.java
+++ wayg/src/main/java/com/ssafy/wayg/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 //		Set max age to 1800 seconds (30 minutes).
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://j7c202.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(6000);
