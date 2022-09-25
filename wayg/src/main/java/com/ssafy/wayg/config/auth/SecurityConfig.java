467c43153c9c695e2c3e3b0997deffe3c29835e2
허은지 <uiu1442@naver.com>
2022-09-25 23:01:03 +0900

    #6 fix 권한 설정

--- wayg/src/main/java/com/ssafy/wayg/config/auth/SecurityConfig.java
+++ wayg/src/main/java/com/ssafy/wayg/config/auth/SecurityConfig.java
@@ -22,8 +22,8 @@ public class SecurityConfig {
                     .authorizeRequests()
                     .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/swagger-ui/**","/swagger-ui","/feed","/feed/**","/place","/place/**").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //USER 권한 가진 사람만 가능 -> 로그인 후 기능 url 넣어야함
-                    .antMatchers("/oauth2/authorization/kakao").permitAll()
-                    .antMatchers("/api/login").permitAll()
+                    .antMatchers("/oauth2/authorization/kakao/**").permitAll()
+                    .antMatchers("/api/login/**").permitAll()
                     .anyRequest().authenticated() //설정값 이외의 url은 인증 완료한 사용자만이 사용가능함
 //                    .anyRequest().permitAll()
                 .and()
