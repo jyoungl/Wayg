fca65c733dfb9966cc44c1bfcc674cc17fa2001c
이성찬 <97578445+seongchanleelee@users.noreply.github.com>
2022-10-04 22:54:51 +0900

    #28 fix css

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -15,7 +15,9 @@ function Login({toDos, addToDo}) {
   const REDIRECT_URI = process.env.REACT_APP_HOST+ "login"
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   // const KAKAO_AUTH_URL = `http://localhost:8080/api/oauth2/authorization/kakao`;
-
+  const kakao_btn = () => {
+    alert("성별, 나이 공개에 동의해야 가입이 가능해요 !")
+  }
 
   return (
     <div className={styles.login}>
@@ -33,7 +35,7 @@ function Login({toDos, addToDo}) {
           </div>
         </div>
         <div className={styles.button}>
-          <a href={KAKAO_AUTH_URL}>
+          <a onClick={kakao_btn} href={KAKAO_AUTH_URL}>
             <img
               src={kakaoBtn}
               className={styles.kakao_btn}
