1d73a87e0385574af1aa37d9b56553c71c1215d9
jyoungl <pmis118@naver.com>
2022-10-04 16:02:48 +0900

    #24 fix 오류수정

--- wayg/src/main/java/com/ssafy/wayg/repository/PlacewordRepository.java
+++ wayg/src/main/java/com/ssafy/wayg/repository/PlacewordRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.wayg.repository;
 import com.ssafy.wayg.entity.Place;
 import com.ssafy.wayg.entity.Placeword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ import java.util.List;
 public interface PlacewordRepository extends JpaRepository<Placeword, Integer> {
     Long countByplacewordWord(String placewordWord);
     List<Placeword> findByplacewordWord(String placewordWord);
-    List<Placeword> findTop3ByplacewordWordInOrderByplacewordCountDesc(List<String> placewordWords);
+    @Query(nativeQuery = true, value = "SELECT * FROM Placeword as p WHERE p.placeword_word IN (:words) ORDER BY p.placeword_count DESC LIMIT 3")
+    List<Placeword> findTop3ByplacewordWordInOrderByplacewordCountDesc(@Param("words") List<String> placewordWords);
 }
