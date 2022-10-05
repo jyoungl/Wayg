bf077624e31e4ba68dfe3854130ccef99c31fe01
lapera00 <86655589+lapera00@users.noreply.github.com>
2022-10-05 16:56:08 +0900

    #27 fix num

--- scheduler/nabcSc.py
+++ scheduler/nabcSc.py
@@ -23,7 +23,7 @@ def travel(word) :
 
 
     quote = word  #검색어
-    display_num = "4"     #최대갯수
+    display_num = "100"     #최대갯수
 
     encText = urllib.parse.quote(quote)
     url = "https://openapi.naver.com/v1/search/blog?query=" + encText +"&display=" + display_num  +"&sort=date" # json 결과
